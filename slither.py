89787002122ccc76c0bfd7e26d827ff6803d2b32
blackbe <blackbe@onid.oregonstate.edu>
2018-09-18 15:44:16 -0700

    Added: 1. Choice to play again or exit. 2. Rendering the apple.

--- slither.py
+++ slither.py
@@ -1,5 +1,6 @@
 import pygame
 import time
+import random
 
 pygame.init()
 
@@ -13,14 +14,6 @@ display_height = 600
 gameDisplay = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption('Slither')
 
-gameExit = False
-
-lead_x = display_width / 2
-lead_y = display_height / 2
-
-lead_x_change = 0
-lead_y_change = 0
-
 clock = pygame.time.Clock()
 
 block_size = 10
@@ -33,37 +26,69 @@ def message_to_screen(msg, color):
     gameDisplay.blit(screen_text, [display_width / 2 - 200, display_height / 2])
 
 
-while not gameExit:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            gameExit = True
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                lead_x_change = -block_size
-                lead_y_change = 0
-            elif event.key == pygame.K_RIGHT:
-                lead_x_change = block_size
-                lead_y_change = 0
-            elif event.key == pygame.K_UP:
-                lead_x_change = 0
-                lead_y_change = -block_size
-            elif event.key == pygame.K_DOWN:
-                lead_x_change = 0
-                lead_y_change = block_size
+def gameLoop():
+    gameExit = False
+    gameOver = False
 
-    if lead_x >= display_width or lead_x < 0 or lead_y >= display_height or lead_y < 0:
-        gameExit = True
+    lead_x = display_width / 2
+    lead_y = display_height / 2
 
-    lead_x += lead_x_change
-    lead_y += lead_y_change
-    gameDisplay.fill(white)
-    pygame.draw.rect(gameDisplay, black, [lead_x, lead_y, block_size, block_size])
+    lead_x_change = 0
+    lead_y_change = 0
+
+    randAppleX = random.randrange(0, display_width - block_size)
+    randAppleY = random.randrange(0, display_height - block_size)
+
+    while not gameExit:
+
+        while gameOver == True:
+            gameDisplay.fill(white)
+            message_to_screen("Game over, press C to play again, or Q to quit", red)
+            pygame.display.update()
+
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        gameExit = True
+                        gameOver = False
+                    elif event.key == pygame.K_c:
+                        gameLoop()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                gameExit = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    lead_x_change = -block_size
+                    lead_y_change = 0
+                elif event.key == pygame.K_RIGHT:
+                    lead_x_change = block_size
+                    lead_y_change = 0
+                elif event.key == pygame.K_UP:
+                    lead_x_change = 0
+                    lead_y_change = -block_size
+                elif event.key == pygame.K_DOWN:
+                    lead_x_change = 0
+                    lead_y_change = block_size
+
+        if lead_x >= display_width or lead_x < 0 or lead_y >= display_height or lead_y < 0:
+            gameOver = True
+
+        lead_x += lead_x_change
+        lead_y += lead_y_change
+        gameDisplay.fill(white)
+        pygame.draw.rect(gameDisplay, red, [randAppleX, randAppleY, block_size, block_size])
+        pygame.draw.rect(gameDisplay, black, [lead_x, lead_y, block_size, block_size])
+        pygame.display.update()
+
+        clock.tick(FPS)
+
+    message_to_screen("Have a wonderful day!", red)
     pygame.display.update()
+    time.sleep(3)  # not going to keep this here
+    pygame.quit()
+    quit()
 
-    clock.tick(FPS)
 
-message_to_screen("You lost.  You should give up and play something else.", red)
-pygame.display.update()
-time.sleep(3)  # not going to keep this here
-pygame.quit()
-quit()
+gameLoop()
+
