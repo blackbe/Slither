134babbe85c89df5b7419981a41923ec457e920b
blackbe <blackbe@onid.oregonstate.edu>
2018-09-18 14:25:05 -0700

    Added: 1. frames per second 2. up and down movement

--- slither.py
+++ slither.py
@@ -6,6 +6,7 @@ white = (255, 255, 255)
 black = (0, 0, 0)
 red = (255, 0, 0)
 
+clock = pygame.time.Clock()
 
 gameDisplay = pygame.display.set_mode((800, 600))
 pygame.display.set_caption('Slither')
@@ -15,6 +16,7 @@ gameExit = False
 lead_x = 300
 lead_y = 300
 lead_x_change = 0
+lead_y_change = 0
 
 while not gameExit:
     for event in pygame.event.get():
@@ -23,13 +25,25 @@ while not gameExit:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 lead_x_change -= 10
-            if event.key == pygame.K_RIGHT:
+                lead_y_change = 0
+            elif event.key == pygame.K_RIGHT:
                 lead_x_change = 10
-    lead_x += lead_x_change
+                lead_y_change = 0
+            elif event.key == pygame.K_UP:
+                lead_x_change = 0
+                lead_y_change -= 10
+            elif event.key == pygame.K_DOWN:
+                lead_x_change = 0
+                lead_y_change = 10
 
+
+    lead_x += lead_x_change
+    lead_y += lead_y_change
     gameDisplay.fill(white)
     pygame.draw.rect(gameDisplay, black, [lead_x, lead_y, 10, 10])
     pygame.display.update()
+
+    clock.tick(30)
 
 
 pygame.quit()
