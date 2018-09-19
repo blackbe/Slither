4013fbd451fec601574cbaf4a6b06cf56e972110
blackbe <blackbe@onid.oregonstate.edu>
2018-09-19 16:09:57 -0700

    Centered the text.

--- slither.py
+++ slither.py
@@ -28,10 +28,15 @@ def snake(block_size, snakeList):
         pygame.draw.rect(gameDisplay, green, [XnY[0], XnY[1], block_size, block_size])
 
 
-def message_to_screen(msg, color):
-    screen_text = font.render(msg, True, color)
-    gameDisplay.blit(screen_text, [display_width / 2 - 200, display_height / 2])
+def text_objects(text, color):
+    textSurface = font.render(text, True, color)
+    return textSurface, textSurface.get_rect()
 
+
+def message_to_screen(msg, color):
+    textSurf, textRect = text_objects(msg,color)
+    textRect.center = (display_width / 2), (display_height / 2)
+    gameDisplay.blit(textSurf, textRect)
 
 def gameLoop():
     gameExit = False
